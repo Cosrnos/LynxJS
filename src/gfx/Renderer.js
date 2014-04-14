291bd7d1301e3ecf1f54cd043a3447d3dcb7fa97
Lios <me@cosrnos.com>
2014-04-14 08:24:43 -0500

    Fixes a bug that would cause the wrong texture to be rendered for an object in some instances.

--- src/gfx/Renderer.js
+++ src/gfx/Renderer.js
@@ -302,6 +302,7 @@ Lynx.Renderer = function(pCanvas){
 				pObjects = [pObjects];
 
 			var lastLayer = 0;
+			var lastTexture = null;
 
 			for(var i = 0; i < pObjects.length; i++)
 			{
@@ -321,37 +322,45 @@ Lynx.Renderer = function(pCanvas){
 
 				if(o.Color.Hex == -1 && o.Texture !== false)
 				{
-					if(o.Texture instanceof Image)
+					if(o.Texture != lastTexture)
 					{
-						var tempText = gl.createTexture();
-						gl.bindTexture(gl.TEXTURE_2D, tempText);
+						renderBatch(buildArray);
+						buildArray = [];				
 
-						gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-						gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-						gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-						gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-						gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
+						if(o.Texture instanceof Image)
+						{
+							var tempText = gl.createTexture();
+							gl.bindTexture(gl.TEXTURE_2D, tempText);
 
-						gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, o.Texture);						
+							gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+							gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+							gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+							gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+							gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
 
-						o.Texture = tempText;
+							gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, o.Texture);						
+
+							o.Texture = tempText;
+						}
+
+						var texCoordLocation = vertexShader.GetVariable("texCoord", "attribute").Location;
+						gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+						gl.enableVertexAttribArray(texCoordLocation);
+						gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+						gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+							0.0, 0.0,
+							0.0, 1.0,
+							1.0, 1.0,
+							1.0, 1.0,
+							1.0, 0.0,
+							0.0, 0.0]), gl.STATIC_DRAW);
+						gl.bindTexture(gl.TEXTURE_2D, o.Texture);
+
+						lastTexture = o.Texture;
+
+						gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+						gl.vertexAttribPointer(vertexShader.GetVariable("position", "attribute").Location, 2, gl.FLOAT, false, 0, 0);
 					}
-
-					var texCoordLocation = vertexShader.GetVariable("texCoord", "attribute").Location;
-					gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-					gl.enableVertexAttribArray(texCoordLocation);
-					gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
-					gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-						0.0, 0.0,
-						0.0, 1.0,
-						1.0, 1.0,
-						1.0, 1.0,
-						1.0, 0.0,
-						0.0, 0.0]), gl.STATIC_DRAW);
-					gl.bindTexture(gl.TEXTURE_2D, o.Texture);
-
-					gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-					gl.vertexAttribPointer(vertexShader.GetVariable("position", "attribute").Location, 2, gl.FLOAT, false, 0, 0);
 				}
 
 				if(o.Color.Hex != lastShaderColor)
